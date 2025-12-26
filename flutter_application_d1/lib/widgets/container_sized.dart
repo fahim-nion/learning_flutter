e232979844fc9dd61c5a9a1a32e89178ac4b0c62
fahim-nion <fahimalhassan4@gmail.com>
2025-12-27 00:49:30 +0600

    updated repo

---
+++ flutter_application_d1/lib/widgets/container_sized.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+
+
+
+
+class Container_Sized extends StatelessWidget {
+  
+  
+  
+  
+  const Container_Sized({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Container and Size Box pintos'),),
+      body: Center(),
+    );
+  }
+}
