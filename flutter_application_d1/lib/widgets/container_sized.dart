ae77c2c67828699d640d5d2786f0ddbf5247f493
fahim-nion <fahimalhassan4@gmail.com>
2025-12-30 15:33:44 +0600

    learning flutter

--- flutter_application_d1/lib/widgets/container_sized.dart
+++ flutter_application_d1/lib/widgets/container_sized.dart
@@ -14,16 +14,18 @@ class Container_Sized extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Container and Size Box'),),
-      body: 
+      appBar: AppBar(title: Text('Container and SizedBox'),),
+      body: Container(
+        height: 80,
+        width: 80,
+        child: Text("Hello Bojack Horseman!"),
+      ),
       //this is how you center a sized box but remember you can not use any color to the sized box
       // Center(
       //   child: SizedBox(
       //     height: 50,
       //     width: 100,
       //     child: Text("Hello World!"),
-      //   ),
-      ),
-    );
+        );
   }
 }
