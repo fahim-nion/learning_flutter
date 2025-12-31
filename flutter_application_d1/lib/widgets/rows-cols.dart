9893a8e97f0760c83ebcb7a8e339d861b997d482
fahim-nion <fahimalhassan4@gmail.com>
2025-12-31 12:28:41 +0600

    learning flutter

--- flutter_application_d1/lib/widgets/rows-cols.dart
+++ flutter_application_d1/lib/widgets/rows-cols.dart
@@ -16,13 +16,27 @@ class RowsCols extends StatelessWidget {
         height: h,
         width: w,
         color: Colors.amber.shade200,
-        child: Row(children: [
-          Container(height: 60, width: 60, color: Colors.red.shade100,),
-          Container(height: 60, width: 60, color: Colors.red.shade200,),
-          Container(height: 60, width: 60, color: Colors.red.shade300,),
-          Container(height: 60, width: 60, color: Colors.red.shade400,),
-          Container(height: 60, width: 60, color: Colors.red.shade500,),
-        ],)
+        child:
+          Column(children: [
+            Container(height: 60, width: 60, color: Colors.green.shade100,),
+            Container(height: 60, width: 60, color: Colors.green.shade200,),
+            Container(height: 60, width: 60, color: Colors.green.shade300,),
+            Container(height: 60, width: 60, color: Colors.green.shade400,),
+            Container(height: 60, width: 60, color: Colors.green.shade500,),
+          ],)
+        // Wrap(
+        //   //for space overlaping issue we can use wrap ,but wrap does not have main or axis allignment. 
+        //   // mainAxisAlignment: MainAxisAlignment.start,
+        //   // crossAxisAlignment: CrossAxisAlignment.center,
+        //   direction: Axis.vertical,
+        //   alignment: WrapAlignment.start,
+        //   children: [
+        //     Container(height: 60, width: 60, color: Colors.red.shade100,),
+        //     Container(height: 60, width: 60, color: Colors.red.shade200,),
+        //     Container(height: 60, width: 60, color: Colors.red.shade300,),
+        //     Container(height: 60, width: 60, color: Colors.red.shade400,),
+        //     Container(height: 60, width: 60, color: Colors.red.shade500,),
+        // ],)
         //  Center(child: Container(
         //   height: 60,
         //   width: 60,
