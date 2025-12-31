081c547475904b46d1792f89633dcaa362c165d4
fahim-nion <fahimalhassan4@gmail.com>
2025-12-31 14:50:51 +0600

    learning flutter

--- flutter_application_d1/lib/widgets/buttons.dart
+++ flutter_application_d1/lib/widgets/buttons.dart
@@ -6,7 +6,28 @@ class Buttons extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Text Buttons and Elevated Buttons"),),
+      appBar: AppBar(title: Text("Text Buttons and Elevated Buttons")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              style: ButtonStyle(
+                overlayColor: WidgetStatePropertyAll(const Color.fromARGB(255, 6, 226, 223) ),
+                elevation: WidgetStatePropertyAll(20),
+                backgroundColor: WidgetStateProperty.all(const Color.fromARGB(255, 1, 154, 103)),
+                foregroundColor: WidgetStatePropertyAll(Colors.black),
+                shape: WidgetStatePropertyAll(
+                  RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                ),
+                padding:WidgetStatePropertyAll(EdgeInsets.all(20)),
+              ),
+              child: Text("Press Me", style: TextStyle(fontSize: 20,)),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
