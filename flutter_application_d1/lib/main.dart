5cc03c141e29e9d5048b2e624d41ba855f371b40
fahim-nion <fahimalhassan4@gmail.com>
2026-01-01 15:51:21 +0600

    learning flutter

--- flutter_application_d1/lib/main.dart
+++ flutter_application_d1/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme:ThemeData(
-        brightness: Brightness.light,
+        brightness: Brightness.dark,
         appBarTheme: AppBarTheme(
           backgroundColor: Colors.deepPurple,
         )
--- flutter_application_d1/lib/widgets/list_grid.dart
+++ flutter_application_d1/lib/widgets/list_grid.dart
@@ -2,110 +2,39 @@ import 'package:flutter/material.dart';
 
 class ListGrid extends StatelessWidget {
   const ListGrid({super.key});
+  final List<String> fruits = const ['Orange','Apfel','Mango','Banana','Jack-fruit']; 
+  final Map fruits_person = const {
+    'fruits': ['Orange','Apfel','Mango','Banana','Jack-fruit'],
+    'names' : ['Abdul','Bodi','Fahim','Rahim','Motiur']
+  };
 
   @override
+  
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("List and Grid"),
-        backgroundColor: Colors.green.shade400,
-        elevation: 8,
+        title: const Text("List and Grid"),
       ),
       body: Container(
-        child: ListView(
-          children: [
-            Card(
-              child: ListTile(
-                title: Text("Orange"),
-                subtitle: Text("Abdul ate this fruit"),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                title: Text("Orange"),
-                subtitle: Text("Abdul ate this fruit"),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                title: Text("Orange"),
-                subtitle: Text("Abdul ate this fruit"),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                title: Text("Orange"),
-                subtitle: Text("Abdul ate this fruit"),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                title: Text("Orange"),
-                subtitle: Text("Abdul ate this fruit"),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                title: Text("Orange"),
-                subtitle: Text("Abdul ate this fruit"),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                title: Text("Orange"),
-                subtitle: Text("Abdul ate this fruit"),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                title: Text("Orange"),
-                subtitle: Text("Abdul ate this fruit"),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                title: Text("Orange"),
-                subtitle: Text("Abdul ate this fruit"),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                title: Text("Orange"),
-                subtitle: Text("Abdul ate this fruit"),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                title: Text("Orange"),
-                subtitle: Text("Abdul ate this fruit"),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                title: Text("Orange"),
-                subtitle: Text("Abdul ate this fruit"),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                title: Text("Orange"),
-                subtitle: Text("Abdul ate this fruit"),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                title: Text("Orange"),
-                subtitle: Text("Abdul ate this fruit"),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                title: Text("Orange"),
-                subtitle: Text("Abdul ate this fruit"),
-              ),
-            ),
-          ],
-        ),
+        // child: ListView.builder(
+        //   itemCount: fruits.length,
+        //   itemBuilder: (context, index) {
+        //     return Card(
+        //       child: ListTile(
+        //         leading: Icon(Icons.person_2),
+        //         onTap: (){
+        //           print(fruits_person['names'][index]);
+        //         },
+        //         title: Text(fruits_person['fruits'][index]),
+        //         subtitle: Text(fruits_person['names'][index]),
+        //       ),
+        //     );
+        //   },
+        // ),
+
+        child: GridView(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2)
+          ),
       ),
     );
   }
@@ -116,3 +45,99 @@ class ListGrid extends StatelessWidget {
 
 
 //but using column will give me an error saying the height is limited where ListView offers a good scrolling effect
+
+//instead of writting hell of a code like this below we use ListBuilder
+// when we exactly dont know the length
+/*Card(
+              child: ListTile(
+                title: Text("Orange"),
+                subtitle: Text("Abdul ate this fruit"),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text("Orange"),
+                subtitle: Text("Abdul ate this fruit"),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text("Orange"),
+                subtitle: Text("Abdul ate this fruit"),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text("Orange"),
+                subtitle: Text("Abdul ate this fruit"),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text("Orange"),
+                subtitle: Text("Abdul ate this fruit"),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text("Orange"),
+                subtitle: Text("Abdul ate this fruit"),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text("Orange"),
+                subtitle: Text("Abdul ate this fruit"),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text("Orange"),
+                subtitle: Text("Abdul ate this fruit"),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text("Orange"),
+                subtitle: Text("Abdul ate this fruit"),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text("Orange"),
+                subtitle: Text("Abdul ate this fruit"),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text("Orange"),
+                subtitle: Text("Abdul ate this fruit"),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text("Orange"),
+                subtitle: Text("Abdul ate this fruit"),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text("Orange"),
+                subtitle: Text("Abdul ate this fruit"),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text("Orange"),
+                subtitle: Text("Abdul ate this fruit"),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text("Orange"),
+                subtitle: Text("Abdul ate this fruit"),
+              ),
+            ),
+            
+            
+            */
